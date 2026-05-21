22bc72579ba24f2d84efd3af76e73134f89ac60f
Sourabh Barwal <sourabh17barwal@gmail.com>
2026-05-22 00:05:03 +0530

    Cookie fix for production.

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -11,7 +11,7 @@ const setRefreshCookie = (res, token) => {
   res.cookie('refreshToken', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
